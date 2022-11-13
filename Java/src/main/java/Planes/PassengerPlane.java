65d7d85240f08f57073f138eabd6cfca586afb48
Ivan <90043490+ivansamas@users.noreply.github.com>
2022-11-13 19:15:53 +0300

    Update PassengerPlane.java

--- Java/src/main/java/Planes/PassengerPlane.java
+++ Java/src/main/java/Planes/PassengerPlane.java
@@ -4,17 +4,13 @@ import java.util.Objects;
 
 public class PassengerPlane extends Plane{
 
-    //=================FIELDS=================
     private int passengersCapacity;
 
-    //=================CONSTRUCTORS=================
     public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.passengersCapacity = passengersCapacity;
     }
 
-
-    //=================METHODS=================
     public int getPassengersCapacity() {
         return passengersCapacity;
     }
@@ -26,20 +22,11 @@ public class PassengerPlane extends Plane{
                 '}');
     }
 
-//    @Override
-//    public String toString() {
-//        return super.toString().replace("}",
-//                ", passengersCapacity=" + passengersCapacity +
-//                        '}');
-//    }
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PassengerPlane)) return false;
-        if (!super.equals(o)) return false;
-        PassengerPlane plane = (PassengerPlane) o;
-        return passengersCapacity == plane.passengersCapacity;
+    public boolean equals(Object anothePassengerPlane) {
+        if (this == anothePassengerPlane) return true; 
+        if (!(o instanceof PassengerPlane) || !super.equals(anothePassengerPlane)) return false; 
+        return passengersCapacity == ((PassengerPlane) anothePassengerPlane).passengersCapacity;
     }
 
     @Override
