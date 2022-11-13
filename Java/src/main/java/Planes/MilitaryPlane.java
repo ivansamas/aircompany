2d4fbad16a642dbe6cf88f9c84bcb043cca2a425
Ivan <90043490+ivansamas@users.noreply.github.com>
2022-11-13 19:13:13 +0300

    Update MilitaryPlane.java

--- Java/src/main/java/Planes/MilitaryPlane.java
+++ Java/src/main/java/Planes/MilitaryPlane.java
@@ -6,35 +6,33 @@ import java.util.Objects;
 
 public class MilitaryPlane extends Plane{
 
-    private MilitaryType type;
+    private MilitaryType militaryType;
 
-    public MilitaryPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type) {
+    public MilitaryPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType militaryType) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+        this.militaryType = militaryType;
     }
 
-    public MilitaryType getType() {
-        return type;
+    public MilitaryType getMilitaryType() {
+        return militaryType;
     }
 
     @Override
     public String toString() {
         return super.toString().replace("}",
-                ", type=" + type +
+                ", type=" + militaryType +
                 '}');
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MilitaryPlane)) return false;
-        if (!super.equals(o)) return false;
-        MilitaryPlane that = (MilitaryPlane) o;
-        return type == that.type;
+    public boolean equals(Object anotherMilitaryPlane) {
+        if (this == anotherMilitaryPlane) return true;
+        if (!(anotherMilitaryPlane instanceof MilitaryPlane) || !super.equals(anotherMilitaryPlane)) { return false; }
+        return militaryType == ((MilitaryPlane) anotherMilitaryPlane).militaryType;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), type);
+        return Objects.hash(super.hashCode(), militaryType);
     }
 }
