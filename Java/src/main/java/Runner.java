d1073ed1e37917e5dbf2fdd3831fcdde3d2e662c
Ivan <90043490+ivansamas@users.noreply.github.com>
2022-11-13 19:34:22 +0300

    Update Runner.java

--- Java/src/main/java/Runner.java
+++ Java/src/main/java/Runner.java
@@ -24,17 +24,10 @@ public class Runner {
             new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
     );
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Airport airport = new Airport(planes);
         Airport militaryAirport = new Airport(airport.getMilitaryPlanes());
-        Airport passengerAirport = new Airport(airport.getPasPl());
-        System.out.println("Military airport sorted by max distance: " + militaryAirport
-                .sortByMaxDistance()
-                .toString());
-        System.out.println("Passenger airport sorted by max speed: " + passengerAirport
-                .sortByMaxSpeed()
-                .toString());
-
-        System.out.println("Plane with max passenger capacity: " + passengerAirport.getPassengerPlaneWithMaxPassengersCapacity());
+        Airport passengerAirport = new Airport(airport.getPassengerPlanes());
     }
 }
