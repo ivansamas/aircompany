68b8689e55429b9fcad24a9aac439e416091579e
Ivan <90043490+ivansamas@users.noreply.github.com>
2022-11-13 19:23:41 +0300

    Update and rename destroyerPlane.java to DestroyerPlane.java

---
+++ Java/src/main/java/Planes/DestroyerPlane.java
@@ -0,0 +1,7 @@
+package Planes;
+
+public class DestroyerPlane  extends Plane {
+    public DestroyerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
--- Java/src/main/java/Planes/destroyerPlane.java
+++
@@ -1,7 +0,0 @@
-package Planes;
-
-public class destroyerPlane  extends Plane {
-    public destroyerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-    }
-}
